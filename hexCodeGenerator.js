64b5485acac431dcaeea29d41c1e10cfdf4c94da
Malick Diakite <malickdiakite496@gmail.com>
2020-03-23 19:16:41 +0530

    First commit

---
+++ hexCodeGenerator.js
@@ -0,0 +1,22 @@
+var body, id;
+
+function changeBg() {
+  body = document.getElementsByTagName("body")[0];
+
+  var hexcolor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+
+  var hex = "#";
+
+  var id = setInterval(generateHexCode, 2000);
+
+  function generateHexCode() {
+    var rand;
+    for (var i = 0; i < 6; i++) {
+      rand = Math.floor(Math.random() * hexcolor.length);
+      hex += hexcolor[rand];
+    }
+    body.style.backgroundColor = hex;
+    document.querySelector(".hex").textContent = hex;
+    hex = "#";
+  }
+}
